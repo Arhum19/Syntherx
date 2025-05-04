2fcad62d078bf44a0246fadf22d581a65568b10e
Arhum19 <arhumnoor10@gmail.com>
2025-05-04 18:21:32 +0500

    nav image changes

---
+++ script.js
@@ -0,0 +1,24 @@
+
+  const fullText = "SyntherX";
+  const target = document.querySelector(".auto-type");
+  let i = 0;
+
+  function type() {
+    if (i < fullText.length) {
+      if (fullText[i] === "X") {
+        target.innerHTML += `<span class="blue">X</span>`;
+      } else {
+        target.innerHTML += fullText[i];
+      }
+      i++;
+      setTimeout(type, 100);
+    } else {
+      setTimeout(() => {
+        target.innerHTML = "";
+        i = 0;
+        type(); // loop again
+      }, 500); // wait before restarting
+    }
+  }
+
+  type();
